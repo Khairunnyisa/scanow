184a0a7a6b4477ad225c7820da207851ff2e2800
“Khairunnyisa” <khairunnyisa@gmail.com>
2025-01-16 11:15:51 +0700

    Perubahan stateful ke stateless

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
 // Kelas Home sebagai StatefulWidget
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
